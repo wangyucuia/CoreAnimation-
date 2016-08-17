97ef58050a1c56582b1c61f176771ec3ce5d8f48
王玉翠 <m15201386258@163.com>
2016-08-17 15:48:58 +0800

    Initial Commit

---
+++ CoreAnimation实现倒影效果/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreAnimation实现倒影效果
+//
+//  Created by 王玉翠 on 16/8/17.
+//  Copyright © 2016年 王玉翠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreAnimation实现倒影效果Tests/CoreAnimation______Tests.m
@@ -0,0 +1,39 @@
+//
+//  CoreAnimation______Tests.m
+//  CoreAnimation实现倒影效果Tests
+//
+//  Created by 王玉翠 on 16/8/17.
+//  Copyright © 2016年 王玉翠. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CoreAnimation______Tests : XCTestCase
+
+@end
+
+@implementation CoreAnimation______Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
